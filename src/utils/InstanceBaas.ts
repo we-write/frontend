2bce190a97c0af519e5589fa787352f240d3e91f
swimmingRiver <swimmingr@gmail.com>
2025-05-16 15:22:08 +0900

    feat: 환경변수 에러 핸들링 추가

--- src/utils/InstanceBaas.ts
+++ src/utils/InstanceBaas.ts
@@ -4,5 +4,7 @@ const instanceBaaS = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BAAS_URL,
   timeout: 5000,
 });
-
+if (!process.env.NEXT_PUBLIC_BAAS_URL) {
+  throw new Error('BaaS URL is not defined');
+}
 export default instanceBaaS;
--- src/utils/instance.ts
+++ src/utils/instance.ts
@@ -4,5 +4,7 @@ const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   timeout: 5000,
 });
-
+if (!process.env.NEXT_PUBLIC_BASE_URL) {
+  throw new Error('Base URL is not defined');
+}
 export default instance;
