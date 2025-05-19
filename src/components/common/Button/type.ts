1018aa8188528a0677c908a907b89525769e166d
myeongheonhong <ruddyh7076@gmail.com>
2025-05-19 13:16:33 +0900

    feat: button컴포넌트 구현

---
+++ src/components/common/Button/type.ts
@@ -0,0 +1,17 @@
+import { ColorType, ColorVariant } from '@/utils/getColorSystem';
+import { ButtonHTMLAttributes } from 'react';
+
+export const BUTTON_SIZE = {
+  custom: '',
+  full: 'w-full h-11',
+} as const;
+
+export type ButtonSize = keyof typeof BUTTON_SIZE;
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: ButtonSize;
+  color: ColorType;
+  variant?: ColorVariant;
+  isDisabled?: boolean;
+  isLoading?: boolean;
+}
