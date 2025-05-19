c93166c47b8b8052cb765cb80e54f7b37feccccd
hoxey2Front <hoxey2react@gmail.com>
2025-05-19 13:33:09 +0900

    feat: layout, GNB 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,9 @@ export default function RootLayout({
         className={`${pretendard.variable} ${hanuman.variable} antialiased`}
       >
         <QueryProviders>
-          <GNB />
+          <header>
+            <GNB />
+          </header>
           <main className="mt-[60px] h-screen w-full bg-gray-100 p-4">
             {children}
           </main>
