9f69a5448762bc451ad38c872304193e59ea4c71
hoxey2Front <hoxey2react@gmail.com>
2025-05-20 16:51:05 +0900

    refactor: 드로어 메뉴 선택 여부 함수화, 로그인 상태 외부로 적용

--- src/components/layout/SideDrawer/SideDrawer.tsx
+++ src/components/layout/SideDrawer/SideDrawer.tsx
@@ -1,67 +1,71 @@
+"use client";
+
 import { SideDrawerProps } from '@/components/layout/SideDrawer/type';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { useState } from 'react';
+import { APP_ROUTES } from '@/constants/appRoutes';
+import { useSignIn } from '@/hooks/useSignIn';
 
 export const SideDrawer = ({
   isOpen,
   closeDrawer,
-  // isSignIn,
   menuItems,
   signInImageSrc,
 }: SideDrawerProps) => {
-  const [isSignIn, toggleSignIn] = useState(false);
   const pathname = usePathname();
+  const { isSignIn, toggleSignIn } = useSignIn();
+
   const handleSignIn = () => {
-    toggleSignIn(!isSignIn);
+    toggleSignIn();
     closeDrawer();
-  }
+  };
+
+  const isMenuActive = (item: { href: string }) => {
+    return pathname === item.href;
+  };
 
   return (
     <div
-      className={`fixed top-0 right-0 z-50 flex h-full w-2/3 transform flex-col gap-4 bg-white p-6 transition-transform duration-300 ease-in-out md:hidden ${isOpen ? 'translate-x-0' : 'translate-x-full'} `}
+      className={`fixed top-0 right-0 z-50 flex h-full w-2/3 transform flex-col gap-4 bg-white p-6 transition-transform duration-300 ease-in-out md:hidden ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}
     >
       <button
-        className="mb-4 cursor-pointer self-end px-6 py-4 text-base font-[600] text-gray-500 rounded-md"
+        className="mb-4 self-end px-6 py-4 text-base font-semibold text-gray-500 rounded-md"
         onClick={closeDrawer}
       >
         ✕
       </button>
-      {isSignIn ? (
+
+      {isSignIn && (
         <Link
-          href="/mypage"
+          href={APP_ROUTES.mypage}
           role="button"
-          className="flex-center cursor-pointer mb-4"
+          className="flex-center mb-4"
         >
           <Image src={signInImageSrc} alt="SignInImg" width={40} height={40} />
-        </Link>) : ""}
+        </Link>
+      )}
 
-      {menuItems.map((item) => {
-        const isActive = pathname === item.href;
+      {menuItems.map((item) => (
+        <Link
+          key={item.href}
+          href={item.href}
+          onClick={closeDrawer}
+          className={`rounded-md p-4 text-start font-semibold text-gray-500 transition duration-300 ${isMenuActive(item) ? 'bg-gray-200' : 'hover:bg-gray-200'}`}
+        >
+          {item.label}
+        </Link>
+      ))}
 
-        return (
-          <Link
-            key={item.href}
-            href={item.href}
-            onClick={closeDrawer}
-            className={`rounded-md p-4 text-start font-[600] text-gray-500 transition duration-300 ${isActive ? 'bg-gray-200' : 'hover:bg-gray-200'} `}
-          >
-            {item.label}
-          </Link>
-        );
-      })}
-      <Link
-        href="/auths/signin">
+      <Link href={APP_ROUTES.signin}>
         <button
           onClick={handleSignIn}
-          type='button'
-          className="w-full cursor-pointer rounded-md p-4 text-start text-base font-[600] text-gray-500 transition duration-300 hover:bg-gray-200 ">
-          {isSignIn ? "로그아웃" : "로그인"}
+          type="button"
+          className="w-full rounded-md p-4 text-start text-base font-semibold text-gray-500 transition duration-300 hover:bg-gray-200"
+        >
+          {isSignIn ? '로그아웃' : '로그인'}
         </button>
       </Link>
-
-
-    </div >
+    </div>
   );
 };
