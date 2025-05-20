b5f1b22a56d2fcace1def9b9092c350b8e73a984
myeongheonhong <ruddyh7076@gmail.com>
2025-05-20 11:10:50 +0900

    feat: 커스텀 스타일 관련 스토리 추가

--- src/components/common/Button/Button.stories.tsx
+++ src/components/common/Button/Button.stories.tsx
@@ -27,7 +27,7 @@ export const WhiteInverted: Story = {
   },
 };
 
-export const Custom: Story = {
+export const CustomSize: Story = {
   args: {
     children: 'Button',
     color: 'primary',
@@ -59,5 +59,14 @@ export const Disabled: Story = {
     color: 'primary',
     variant: 'default',
     isDisabled: true,
+  },
+};
+
+export const CustomColor: Story = {
+  args: {
+    children: 'Button',
+    color: 'custom',
+    variant: 'default',
+    className: 'bg-blue-500',
   },
 };
