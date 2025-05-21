fd021094c97f5a357a2855fd4ef1fee8eb49e3af
swimmingRiver <swimmingr@gmail.com>
2025-05-21 11:32:56 +0900

    fix: 타입명 변경

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,8 +1,8 @@
-export interface FormData {
+export interface SignUpFormData {
   name: string;
   email: string;
   password: string;
   passwordCheck: string;
   companyName: string;
 }
-export type SignUpRequest = Omit<FormData, 'passwordCheck'>;
+export type SignUpRequest = Omit<SignUpFormData, 'passwordCheck'>;
