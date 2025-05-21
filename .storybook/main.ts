b3f0cbd720a6295acee6ba1e6f964fffcc6cec58
juyesu <dong4757@naver.com>
2025-05-21 18:50:57 +0900

    chore: 스토리 북에서 여러 뷰포트를 적용할 수 있도록 전역 설정 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,6 +4,7 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-essentials',
+    '@storybook/addon-viewport',
     '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
     '@storybook/experimental-addon-test',
