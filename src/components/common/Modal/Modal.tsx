bcb0d47fd5ea182be986e6f3645ff82d7cc9168a
hoxey2Front <hoxey2react@gmail.com>
2025-05-23 00:29:02 +0900

    refactor: 코드인벤션 맞게 수정, 스타일 변수화

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -5,6 +5,7 @@ import {
   ModalFooterProps,
 } from './type';
 import React from 'react';
+
 export const Modal = ({
   isOpen,
   onClose,
@@ -14,21 +15,19 @@ export const Modal = ({
   children,
   className,
 }: ModalProps) => {
-  const backdropStyle = 'fixed inset-0 z-50 flex items-center justify-center';
-  const modalStyle = 'min-w-75 w-[450px] p-6 bg-white shadow-xl relative';
   if (!isOpen) return null;
+  const backdropStyle =
+    'fixed inset-0 z-50 flex items-center justify-center visible opacity-100"';
+  const backdropColor = noBackdrop ? 'bg-transparent' : 'bg-black/50';
+  const modalStyle = 'min-w-75 w-[450px] p-6 bg-white shadow-xl relative';
+  const fullScreenStyle = fullScreen
+    ? 'flex h-screen w-screen flex-col rounded-none'
+    : 'rounded-xl';
   return (
-    <div
-      onClick={onClose} // 바깥 영역 클릭 시 닫기 (선택사항)
-      className={`${backdropStyle} ${
-        isOpen
-          ? 'visible opacity-100'
-          : 'pointer-events-none invisible opacity-0'
-      } ${noBackdrop ? 'bg-transparent' : 'bg-black/50'}`}
-    >
+    <div className={`${backdropStyle} ${backdropColor}`}>
       <div
-        onClick={(e) => e.stopPropagation()} // 모달 내부 클릭은 닫기 방지
-        className={`${modalStyle} ${fullScreen ? 'flex h-screen w-screen flex-col rounded-none' : 'rounded-xl'} ${className}`}
+        onClick={(e) => e.stopPropagation()}
+        className={`${modalStyle} ${fullScreenStyle} ${className}`}
       >
         {noCloseButton ? null : <CloseButton onClick={onClose} />}
         {children}
@@ -38,10 +37,10 @@ export const Modal = ({
 };
 
 export const CloseButton = ({ onClick }: { onClick: () => void }) => {
-  const CloseButtonStyle = 'w-6 h-6 flex-center text-xl text-gray-500';
+  const closeButtonStyle = 'w-6 h-6 flex-center text-xl text-gray-500';
   return (
     <div className="absolute top-6 right-6">
-      <button type="button" onClick={onClick} className={`${CloseButtonStyle}`}>
+      <button type="button" onClick={onClick} className={`${closeButtonStyle}`}>
         ⨉
       </button>
     </div>
@@ -54,12 +53,11 @@ export const ModalHeader = ({
   children,
   className,
 }: ModalHeaderProps) => {
-  const ModalHeaderStyle =
-    'text-gray-900 text-lg font-semibold flex w-18/19 truncate overflow-x-auto';
+  const modalHeaderStyle =
+    'text-gray-900 text-lg font-semibold flex w-18/19 truncate overflow-x-auto pl-6';
+  const align = start ? 'justify-start' : center ? 'justify-center' : null;
   return (
-    <div
-      className={`${ModalHeaderStyle} ${start ? 'justify-start' : null} ${center ? 'justify-center' : null} ${className}`}
-    >
+    <div className={`${modalHeaderStyle} ${align} ${className}`}>
       {children}
     </div>
   );
@@ -70,27 +68,25 @@ export const ModalContent = ({
   className,
   fullScreen,
 }: ModalContentProps) => {
-  const base = 'text-gray-800 text-base font-medium max-h-120 overflow-y-auto';
-  const fullScreenStyle =
-    'flex-1 flex items-center justify-center flex-col max-h-175 my-auto';
-  const normalStyle = 'flex-center flex-col mt-6 mb-6';
-
+  const baseStyle =
+    'text-gray-800 text-base font-medium max-h-120 overflow-y-auto';
+  const fullScreenStyle = fullScreen
+    ? 'flex-1 flex items-center justify-center flex-col max-h-175 my-auto'
+    : 'flex-center flex-col mt-6 mb-6';
   return (
-    <div
-      className={`${base} ${fullScreen ? fullScreenStyle : normalStyle} ${className}`}
-    >
+    <div className={`${baseStyle} ${fullScreenStyle} ${className}`}>
       {children}
     </div>
   );
 };
 
 export const ModalFooter = ({ children, className }: ModalFooterProps) => {
-  const ModalFooterStyle = 'text-base text-gray-800 font-semibold flex gap-2';
+  const modalFooterStyle = 'text-base text-gray-800 font-semibold flex gap-2';
   const childrenCount = React.Children.count(children);
   const alignment =
     childrenCount > 1 ? 'justify-center' : 'justify-end md:justify-center';
   return (
-    <div className={`${ModalFooterStyle} ${alignment} ${className} mt-auto`}>
+    <div className={`${modalFooterStyle} ${alignment} ${className} mt-auto`}>
       {children}
     </div>
   );
