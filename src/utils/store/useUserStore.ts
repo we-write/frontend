d6cebedeefbd6f0e24de7825d7609caad824e57c
swimmingRiver <swimmingr@gmail.com>
2025-05-26 11:12:10 +0900

    feat: zustand persist 추가

--- src/utils/store/useUserStore.ts
+++ src/utils/store/useUserStore.ts
@@ -1,16 +1,24 @@
 import { UserResponse } from '@/types/user';
 import { create } from 'zustand';
-
+import { createJSONStorage, persist } from 'zustand/middleware';
 interface UserStore {
   user: UserResponse | null;
   setUser: (user: UserResponse) => void;
   resetUser: () => void;
 }
 
-const useUserStore = create<UserStore>((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-  resetUser: () => set({ user: null }),
-}));
+const useUserStore = create<UserStore>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+      resetUser: () => set({ user: null }),
+    }),
+    {
+      name: 'user',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
 
 export default useUserStore;
