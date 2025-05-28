c418b5bdae6f9219f08308968215e931d6cc5c70
juyesu <dong4757@naver.com>
2025-05-28 19:13:50 +0900

    feat: editorToolbar 컴포넌트 구현

---
+++ src/components/common/TextEditor/EditorToolbar.tsx
@@ -0,0 +1,229 @@
+'use client';
+
+import {
+  Bold,
+  Italic,
+  Baseline,
+  Quote,
+  AlignLeft,
+  AlignCenter,
+  AlignRight,
+} from 'lucide-react';
+import Dropdown from '@/components/common/Dropdown/Dropdown';
+import useBoolean from '@/hooks/useBoolean';
+import { useEffect, useState } from 'react';
+import {
+  EditorToolbarPreps,
+  HandleChangeTextAlignParams,
+  HandleFontSizeChangeParams,
+  HandleMarkChangeParams,
+} from '@/components/common/TextEditor/type';
+
+const ALIGN_OPTIONS = ['left', 'center', 'right'] as const;
+const FONT_SIZE_OPTIONS = [
+  '12px',
+  '14px',
+  '16px',
+  '18px',
+  '20px',
+  '24px',
+  '28px',
+  '32px',
+];
+const TOOLBAR_ICON_SIZE_PX = 18;
+
+const EditorToolbar = ({ editor }: EditorToolbarPreps) => {
+  const { value, toggle, setFalse } = useBoolean();
+  const [currentFontSize, setCurrentFontSize] = useState('16px');
+  const [isApplied, setIsApplied] = useState({
+    textBlack: true,
+    textRed: false,
+    bold: false,
+    italic: false,
+    blockquote: false,
+    align: {
+      left: false,
+      center: false,
+      right: false,
+    },
+  });
+  const DropDownContentStyle =
+    'h-full w-full rounded-lg px-4 py-1.5 hover:bg-gray-200';
+
+  useEffect(() => {
+    if (!editor) return;
+
+    const updateMarkStates = () => {
+      const newFontSize = editor.getAttributes('textStyle').fontSize;
+      const newIsApplied = {
+        textBlack: editor.getAttributes('textStyle').color === '#00',
+        textRed: editor.getAttributes('textStyle').color === '#f00',
+        bold: editor.isActive('bold'),
+        italic: editor.isActive('italic'),
+        blockquote: editor.isActive('blockquote'),
+        align: {
+          left: editor.isActive({ textAlign: 'left' }),
+          center: editor.isActive({ textAlign: 'center' }),
+          right: editor.isActive({ textAlign: 'right' }),
+        },
+      };
+      setCurrentFontSize(newFontSize ? newFontSize : '16px');
+      setIsApplied(newIsApplied);
+    };
+
+    editor.on('selectionUpdate', updateMarkStates);
+
+    updateMarkStates();
+
+    return () => {
+      editor.off('selectionUpdate', updateMarkStates);
+    };
+  }, [editor]);
+
+  if (!editor) return null;
+
+  const handleFontSizeChange = ({ fontSize }: HandleFontSizeChangeParams) => {
+    if (fontSize === currentFontSize) {
+      setFalse();
+      return;
+    }
+
+    editor.chain().focus().setFontSize(fontSize).run();
+    setCurrentFontSize(fontSize);
+    setFalse();
+  };
+
+  const handleChangeTextAlign = ({
+    alignDirection,
+  }: HandleChangeTextAlignParams) => {
+    const editorStyles = {
+      fontSize: editor.getAttributes('textStyle').fontSize,
+      color: editor.getAttributes('textStyle').color,
+      isBold: editor.isActive('bold'),
+      isItalic: editor.isActive('italic'),
+    };
+
+    let chain = editor.chain().focus().setTextAlign(alignDirection);
+
+    if (editorStyles.isBold) chain = chain.setMark('bold', {});
+    if (editorStyles.isItalic) chain = chain.setMark('italic', {});
+    if (editorStyles.fontSize) chain = chain.setFontSize(editorStyles.fontSize);
+    if (editorStyles.color) chain = chain.setColor(editorStyles.color);
+
+    chain.run();
+
+    setIsApplied((prev) => ({
+      ...prev,
+      align: {
+        left: editor.isActive({ textAlign: 'left' }),
+        center: editor.isActive({ textAlign: 'center' }),
+        right: editor.isActive({ textAlign: 'right' }),
+      },
+    }));
+
+    setFalse();
+  };
+
+  const handleMarkChange = ({ mark }: HandleMarkChangeParams) => {
+    const chain = editor.chain().focus();
+    setIsApplied((prev) => ({
+      ...prev,
+      [mark]: !prev[mark],
+    }));
+
+    switch (mark) {
+      case 'bold':
+        chain.toggleBold().run();
+        break;
+      case 'italic':
+        chain.toggleItalic().run();
+        break;
+      case 'blockquote':
+        chain.toggleBlockquote().run();
+        break;
+      default:
+        return;
+    }
+    setFalse();
+  };
+
+  return (
+    <div className="flex flex-wrap items-center gap-x-5 gap-y-4 border-b border-b-gray-300 p-2 pt-0 sm:gap-7">
+      <Dropdown
+        trigger={<button onClick={toggle}>{currentFontSize}</button>}
+        isOpen={value}
+      >
+        <Dropdown.Container className="fixed z-10 shadow-md">
+          {FONT_SIZE_OPTIONS.map((size) => (
+            <Dropdown.Content
+              key={size}
+              onClick={() => handleFontSizeChange({ fontSize: size })}
+              contentItem={<div className={DropDownContentStyle}>{size}</div>}
+            />
+          ))}
+        </Dropdown.Container>
+      </Dropdown>
+      <button
+        onClick={() => {
+          editor.chain().focus().setColor('#000').run();
+          setIsApplied((prev) => ({
+            ...prev,
+            textRed: false,
+            textBlack: true,
+          }));
+          setFalse();
+        }}
+        className={`rounded p-1.5 ${isApplied.textBlack && 'bg-gray-200'}`}
+      >
+        <Baseline size={TOOLBAR_ICON_SIZE_PX} />
+      </button>
+      <button
+        onClick={() => {
+          editor.chain().focus().setColor('#f00').run();
+          setIsApplied((prev) => ({
+            ...prev,
+            textBlack: false,
+            textRed: true,
+          }));
+          setFalse();
+        }}
+        className={`rounded p-1.5 ${isApplied.textRed && 'bg-gray-200'}`}
+      >
+        <Baseline size={TOOLBAR_ICON_SIZE_PX} className="text-red-600" />
+      </button>
+      <button
+        onClick={() => handleMarkChange({ mark: 'bold' })}
+        className={`rounded p-1.5 ${isApplied.bold && 'bg-gray-200'}`}
+      >
+        <Bold size={TOOLBAR_ICON_SIZE_PX} />
+      </button>
+      <button
+        onClick={() => handleMarkChange({ mark: 'italic' })}
+        className={`rounded p-1.5 ${isApplied.italic && 'bg-gray-200'}`}
+      >
+        <Italic size={TOOLBAR_ICON_SIZE_PX} />
+      </button>
+      <button
+        onClick={() => handleMarkChange({ mark: 'blockquote' })}
+        className={`rounded p-1.5 ${isApplied.blockquote && 'bg-gray-200'}`}
+      >
+        <Quote size={TOOLBAR_ICON_SIZE_PX} />
+      </button>
+      {ALIGN_OPTIONS.map((direction) => (
+        <button
+          key={direction}
+          onClick={() => handleChangeTextAlign({ alignDirection: direction })}
+          className={`rounded p-1.5 ${isApplied.align[direction] && 'bg-gray-200'}`}
+        >
+          {direction === 'left' && <AlignLeft size={TOOLBAR_ICON_SIZE_PX} />}
+          {direction === 'center' && (
+            <AlignCenter size={TOOLBAR_ICON_SIZE_PX} />
+          )}
+          {direction === 'right' && <AlignRight size={TOOLBAR_ICON_SIZE_PX} />}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default EditorToolbar;
