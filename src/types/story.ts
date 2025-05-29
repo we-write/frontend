3b9c7ac479824c8d35248e61f3473152bfa86870
swimmingRiver <swimmingr@gmail.com>
2025-05-29 15:51:33 +0900

    feat: 스토리 타입 추가

---
+++ src/types/story.ts
@@ -0,0 +1,10 @@
+export interface Story {
+  id: string;
+  author_id: string;
+  title: string;
+  cover_image_url: string;
+  summary: string;
+  genre: string;
+  createdAt: string;
+  updatedAt: string;
+}
