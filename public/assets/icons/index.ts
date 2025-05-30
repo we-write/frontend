7aa664a82c9cffbd265f3035f45bedcf97787718
hoxey2Front <hoxey2react@gmail.com>
2025-05-30 14:31:13 +0900

    feat: 아이콘 이미지 svgr 세팅

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -4,3 +4,5 @@ export { default as VisibilityOn } from './VisibilityOn.svg';
 export { default as Hamburger } from './Hamburger.svg';
 export { default as DefaultProfileImage } from './DefaultProfileImage.svg';
 export { default as Person } from './Person.svg';
+export { default as BtnEditLarge } from './BtnEditLarge.svg';
+export { default as BtnEditSmall } from './BtnEditSmall.svg';
