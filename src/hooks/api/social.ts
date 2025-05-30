78ac368301bbdaa65271ceb9154267347b4f1cae
myeongheonhong <ruddyh7076@gmail.com>
2025-05-30 13:48:48 +0900

    feat: 무한스크롤 useInfiniteQuery로 구현 및 쿼리키 상수로 구분

---
+++ src/hooks/api/social.ts
@@ -0,0 +1,31 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { QUERY_KEY } from '@/constants/queryKey';
+import { GetSocialListParams } from '@/api/social/type';
+import { SocialResponse } from '@/api/social/type';
+import { FETCH_GET_ITEM_LIMIT, getSocialList } from '@/api/social/api';
+
+export const useGetSocialList = (filter: GetSocialListParams) => {
+  return useInfiniteQuery<SocialResponse[]>({
+    queryKey: [QUERY_KEY.SOCIAL, filter],
+    queryFn: async ({ pageParam }) => {
+      try {
+        const response = await getSocialList({
+          ...filter,
+          offset: pageParam as number,
+          limit: FETCH_GET_ITEM_LIMIT,
+        });
+        return response;
+      } catch (error) {
+        console.error('소셜 데이터 조회 실패:', error);
+        throw error;
+      }
+    },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => {
+      if (!lastPage || lastPage.length < FETCH_GET_ITEM_LIMIT) return;
+      return allPages.length * FETCH_GET_ITEM_LIMIT;
+    },
+    // staleTime: 0,
+    // gcTime: 0,
+  });
+};
