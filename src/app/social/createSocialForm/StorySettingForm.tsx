e8fc6eb79c344433ae0155b01e36c21b1053dc28
myeongheonhong <ruddyh7076@gmail.com>
2025-06-03 18:15:01 +0900

    style: 허용인원 1-3명으로 변경

--- src/app/social/createSocialForm/StorySettingForm.tsx
+++ src/app/social/createSocialForm/StorySettingForm.tsx
@@ -47,8 +47,6 @@ const StorySettingForm = ({ methods }: StorySettingFormProps) => {
     { value: 1, label: '1명' },
     { value: 2, label: '2명' },
     { value: 3, label: '3명' },
-    { value: 4, label: '4명' },
-    { value: 5, label: '5명' },
   ];
 
   return (
