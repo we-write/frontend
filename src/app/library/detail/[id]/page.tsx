9d6de1794fc51372a1465a8a0dc58040349a1ca5
swimmingRiver <swimmingr@gmail.com>
2025-06-03 15:34:41 +0900

    feat: 표지 이미지 추가

--- src/app/library/detail/[id]/page.tsx
+++ src/app/library/detail/[id]/page.tsx
@@ -6,6 +6,7 @@ import { useParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import ContentComponent from '@/components/feature/library/ContentComponent';
 import { PaginationControl } from '@/components/feature/library/PaginationControl';
+import Image from 'next/image';
 export interface Content {
   content_id: string;
   content: string;
@@ -15,9 +16,10 @@ const StoryDetailPage = () => {
   const [isMobile, setIsMobile] = useState(false);
   useEffect(() => {
     setIsMobile(window.innerWidth < 640);
-  }, [window.innerWidth]);
+  }, [window?.innerWidth]);
   const { data: story } = useGetStory(id as string);
-  const [page, setPage] = useState(1);
+  console.log(story);
+  const [page, setPage] = useState(0);
   const { data: contents } = useGetContent({
     id: id as string,
     page: page,
@@ -40,24 +42,44 @@ const StoryDetailPage = () => {
 
   return (
     <div className="flex min-h-full w-full flex-col items-center bg-white">
-      <div className="relative flex h-[80dvh] w-[95%] max-w-[1600px] flex-col md:h-[740px]">
-        <div className="relative mt-8 flex-1 md:flex">
-          <section className="relative h-[700px] w-full overflow-y-auto border-r border-gray-200 px-8 py-8 sm:h-[600px] md:w-1/2">
-            <ContentComponent contents={leftPageContents} />
-          </section>
-
-          <section className="absolute top-0 right-0 hidden h-full w-1/2 overflow-y-auto px-8 py-8 md:block">
-            <ContentComponent contents={rightPageContents} />
-          </section>
+      {page === 0 ? (
+        <div className="flex h-[80dvh] w-[95%] flex-col items-center justify-center">
+          <div className="relative aspect-square max-h-[50vh] min-h-[200px] w-[80%] max-w-[500px] min-w-[200px]">
+            {story?.cover_image_url && (
+              <Image
+                src={story?.cover_image_url ?? ''}
+                alt={story?.title ?? '스토리 이미지'}
+                fill
+                className="rounded-lg object-contain"
+                priority
+              />
+            )}
+          </div>
+          <div className="mt-8 flex h-[100px] items-center justify-center">
+            <strong className="text-2xl font-bold text-gray-800">
+              {story?.title}
+            </strong>
+          </div>
         </div>
+      ) : (
+        <div className="relative flex h-[80dvh] w-[95%] max-w-[1600px] flex-col md:h-[740px]">
+          <div className="relative mt-8 flex-1 md:flex">
+            <section className="relative h-[700px] w-full overflow-y-auto border-r border-gray-200 px-8 py-8 sm:h-[600px] md:w-1/2">
+              <ContentComponent contents={leftPageContents} />
+            </section>
 
-        <PaginationControl
-          title={story?.title}
-          page={page}
-          totalPage={totalPage}
-          setPage={setPage}
-        />
-      </div>
+            <section className="absolute top-0 right-0 hidden h-full w-1/2 overflow-y-auto px-8 py-8 md:block">
+              <ContentComponent contents={rightPageContents} />
+            </section>
+          </div>
+        </div>
+      )}{' '}
+      <PaginationControl
+        title={story?.title}
+        page={page}
+        totalPage={totalPage}
+        setPage={setPage}
+      />
     </div>
   );
 };
