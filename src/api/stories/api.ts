11d939c143a36eea26f84ed8aac47917d52a24df
hoxey2Front <hoxey2react@gmail.com>
2025-06-04 13:58:39 +0900

    feat: 무한 스크롤 및 api 커스텀 세팅

--- src/api/stories/api.ts
+++ src/api/stories/api.ts
@@ -1,12 +1,29 @@
 import { Story } from '@/types/story';
 import instanceBaaS from '../instanceBaaS';
 
-export const getStories = async () => {
-  const { data, error } = await instanceBaaS.from('Stories').select('*');
+export const getStories = async ({
+  keyword,
+  offset,
+  limit,
+}: {
+  keyword: string;
+  offset: number;
+  limit: number;
+}) => {
+  const from = offset * limit;
+  const to = from + limit - 1;
 
-  if (error) {
-    throw new Error(error.message);
+  let query = instanceBaaS.from('Stories').select('*');
+
+  if (keyword.trim() !== '') {
+    query = query.ilike('title', `%${keyword}%`);
   }
+
+  query = query.order('created_at', { ascending: false }).range(from, to);
+
+  const { data, error } = await query;
+
+  if (error) throw new Error(error.message);
   return data;
 };
 
