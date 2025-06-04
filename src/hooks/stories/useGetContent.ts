3c0025392c3a8910c3b2b599d9f889639947e949
swimmingRiver <swimmingr@gmail.com>
2025-06-04 16:45:59 +0900

    chore: 타입 경로 수정

--- src/hooks/stories/useGetContent.ts
+++ src/hooks/stories/useGetContent.ts
@@ -1,4 +1,5 @@
-import { getContents, GetContentsProps } from '@/api/stories/api';
+import { getContents } from '@/api/stories/api';
+import { GetContentsProps } from '@/api/stories/type';
 import { useQuery } from '@tanstack/react-query';
 
 export const useGetContent = ({ id, page, limit }: GetContentsProps) => {
