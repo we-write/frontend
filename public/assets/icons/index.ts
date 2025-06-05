3ed2ce5dc1b490b57727440ca3c2056ebecc9150
myeongheonhong <ruddyh7076@gmail.com>
2025-06-05 11:26:23 +0900

    style: 글쓰기 아이콘 추가

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -8,3 +8,4 @@ export { default as BtnEditSmall } from './BtnEditSmall.svg';
 export { default as DownIcon } from './DownIcon.svg';
 export { default as ArrowSort } from './ArrowSort.svg';
 export { default as TrashCan } from './TrashCan.svg';
+export { default as ContentWriteIcon } from './ContentWriteIcon.svg';
