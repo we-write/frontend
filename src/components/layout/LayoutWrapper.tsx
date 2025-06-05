ea5e6e77653108f8a23d9431ce34bc6c12f778e0
myeongheonhong <ruddyh7076@gmail.com>
2025-06-05 14:34:20 +0900

    fix: 화면 전체로 커버될 수 있도록 높이 설정 변경

--- src/components/layout/LayoutWrapper.tsx
+++ src/components/layout/LayoutWrapper.tsx
@@ -8,9 +8,9 @@ interface LayoutWrapperProps {
 const LayoutWrapper = ({ children }: LayoutWrapperProps) => {
   const pathname = usePathname();
   const isAuths = pathname.startsWith('/auths');
-  const basicStyle = 'mx-auto w-full max-w-300 px-4 pt-20 min-h-lvh';
+  const basicStyle = 'mx-auto w-full max-w-300 px-4 pt-20 min-h-lvh h-full';
   return (
-    <div className="flex-1 bg-gray-100">
+    <div className="h-screen flex-1 bg-gray-100">
       <div className={`${basicStyle} ${isAuths ? null : 'bg-gray-50'}`}>
         {children}
       </div>
