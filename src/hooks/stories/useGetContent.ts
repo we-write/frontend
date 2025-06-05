8e075698fabb3a20bdfb4d0ea7a221feede7cc24
myeongheonhong <ruddyh7076@gmail.com>
2025-06-05 11:33:33 +0900

    style: import 해오는 부분 경로 수정

--- src/hooks/stories/useGetContent.ts
+++ src/hooks/stories/useGetContent.ts
@@ -1,4 +1,6 @@
-import { getContents, GetContentsProps } from '@/api/stories/api';
+import { getContents } from '@/api/stories/api';
+// MEMO : import 빌드 에러나서 호출하는 부분 변경했습니다.
+import { GetContentsProps } from '@/api/stories/type';
 import { useQuery } from '@tanstack/react-query';
 
 export const useGetContent = ({ id, page, limit }: GetContentsProps) => {
