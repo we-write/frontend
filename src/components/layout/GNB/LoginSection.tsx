7e7c0373a58f684ef0e7a7a460136dfa9bdbcbf7
swimmingRiver <swimmingr@gmail.com>
2025-06-05 17:03:58 +0900

    fix: 프로필 이미지 rounded 설정

--- src/components/layout/GNB/LoginSection.tsx
+++ src/components/layout/GNB/LoginSection.tsx
@@ -34,6 +34,7 @@ export const LoginSection = () => {
       {userInfo && isSignIn ? (
         userInfo.image ? (
           <Image
+            className="h-14 w-14 rounded-full object-cover"
             src={userInfo.image}
             alt="SIGN_IN_IMAGE"
             width={40}
