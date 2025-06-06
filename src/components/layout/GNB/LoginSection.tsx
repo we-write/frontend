453b1762bd825f80f1387e02f1a4eca2ef5fea82
swimmingRiver <swimmingr@gmail.com>
2025-06-06 14:04:56 +0900

    feat: 컴포넌트 props 반영

--- src/components/layout/GNB/LoginSection.tsx
+++ src/components/layout/GNB/LoginSection.tsx
@@ -3,26 +3,20 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { APP_ROUTES } from '../../../constants/appRoutes';
 import { DefaultProfileImage } from '@public/assets/icons';
+import { UserResponse } from '@/types/user';
 
-import { useEffect, useState } from 'react';
-import { useGetMyInfo } from '@/hooks/api/users/useGetMyInfo';
-
-export const LoginSection = () => {
+export const LoginSection = ({
+  isSignIn,
+  userInfo,
+}: {
+  isSignIn: boolean;
+  userInfo: UserResponse | null;
+}) => {
   const router = useRouter();
-  const [isSignIn, setIsSignIn] = useState<boolean | null>(null);
-  const { data: userInfo } = useGetMyInfo(isSignIn ?? false);
-
-  useEffect(() => {
-    const checkSignInStatus = () => {
-      const localStorageData = localStorage.getItem('isSignIn') === 'true';
-      setIsSignIn(localStorageData);
-    };
-
-    checkSignInStatus();
-  }, [userInfo]);
-
+  console.log({ userInfo });
+  console.log({ isSignIn });
   const handleSignIn = () => {
-    if (isSignIn) {
+    if (userInfo) {
       router.push(APP_ROUTES.mypage);
     } else {
       router.push(APP_ROUTES.signin);
