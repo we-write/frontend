ce9734049cbe3c91ac7476b8cd0fd880b7a7d578
myeongheonhong <ruddyh7076@gmail.com>
2025-06-07 10:08:31 +0900

    refactor: 상수 외부로 분리하여 선언

--- src/app/social/createSocialForm/StorySettingForm.tsx
+++ src/app/social/createSocialForm/StorySettingForm.tsx
@@ -1,5 +1,10 @@
 'use client';
 
+import {
+  APPROVAL_PERIOD_OPTIONS,
+  APPROVER_COUNT_OPTIONS,
+  WORD_LIMIT_OPTIONS,
+} from '@/constants/social/createSocialForm';
 import DropdownInput from './DropdownInput';
 import { PublicCheckboxProps, StorySettingFormProps } from './type';
 
@@ -25,29 +30,6 @@ const StorySettingForm = ({ methods }: StorySettingFormProps) => {
     control,
     formState: { errors },
   } = methods;
-
-  const APPROVAL_PERIOD_OPTIONS = [
-    { value: 1, label: '1일' },
-    { value: 2, label: '2일' },
-    { value: 3, label: '3일' },
-    { value: 4, label: '4일' },
-    { value: 5, label: '5일' },
-    { value: 6, label: '6일' },
-    { value: 7, label: '7일' },
-  ];
-
-  const WORD_LIMIT_OPTIONS = [
-    { value: 150, label: '150자' },
-    { value: 200, label: '200자' },
-    { value: 250, label: '250자' },
-    { value: 300, label: '300자' },
-  ];
-
-  const APPROVER_COUNT_OPTIONS = [
-    { value: 1, label: '1명' },
-    { value: 2, label: '2명' },
-    { value: 3, label: '3명' },
-  ];
 
   return (
     <div className="flex flex-col gap-4">
