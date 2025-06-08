bdd696b997023eb81b9a9ce730ab7ff3c54b3ad9
hoxey2Front <159544052+hoxey2Front@users.noreply.github.com>
2025-06-08 13:37:29 +0900

    feat: 프로필 클릭시 드랍다운 메뉴 기능 추가

---
+++ src/components/layout/GNB/UserDropdown.tsx
@@ -0,0 +1,35 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { APP_ROUTES } from '@/constants/appRoutes';
+import { UserDropdownProps } from './type';
+
+const UserDropdown = ({ onSignOut, onClose }: UserDropdownProps) => {
+  const router = useRouter();
+  const handleSignOut = () => {
+    onSignOut();
+    onClose();
+  };
+  const handleMyPage = () => {
+    router.push(APP_ROUTES.mypage);
+    onClose();
+  };
+
+  return (
+    <div className="absolute top-13 right-2 z-10 mt-2 w-40 overflow-hidden rounded-xl bg-gray-50 shadow-lg">
+      <button
+        onClick={() => handleMyPage()}
+        className="block w-full px-4 py-4 text-left text-sm hover:bg-gray-100"
+      >
+        마이페이지
+      </button>
+      <button
+        onClick={handleSignOut}
+        className="block w-full px-4 py-4 text-left text-sm hover:bg-gray-100"
+      >
+        로그아웃
+      </button>
+    </div>
+  );
+};
+
+export default UserDropdown;
