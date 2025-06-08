891129601d730152253e0422a800ff386954d29e
hoxey2Front <159544052+hoxey2Front@users.noreply.github.com>
2025-06-08 13:38:39 +0900

    refactor: 로그인, 로그아웃 관련 인식 방식 수정

--- src/components/layout/GNB/LoginSection.tsx
+++ src/components/layout/GNB/LoginSection.tsx
@@ -1,51 +1,62 @@
 'use client';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { APP_ROUTES } from '../../../constants/appRoutes';
 import { DefaultProfileImage } from '@public/assets/icons';
 
-import { useEffect, useState } from 'react';
+import { RefObject, useEffect, useRef, useState } from 'react';
 import { useGetMyInfo } from '@/hooks/api/users/useGetMyInfo';
+import UserDropdown from '@/components/layout/GNB/UserDropdown';
+import useBoolean from '@/hooks/useBoolean';
+import { useClickOutside } from '@/hooks/useClickOutside';
+import { usePostSignout } from '@/hooks/api/users/usePostSignout';
 
 export const LoginSection = () => {
-  const router = useRouter();
   const [isSignIn, setIsSignIn] = useState(false);
   const { data: userInfo } = useGetMyInfo(isSignIn);
+  const { value: isDropdownOpen, setTrue: openDropdown, setFalse: closeDropdown } = useBoolean();
+  const { mutate: signout } = usePostSignout();
+
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  useClickOutside(containerRef as RefObject<HTMLElement>, closeDropdown);
 
   useEffect(() => {
-    const checkSignInStatus = () => {
-      const localStorageData = localStorage.getItem('isSignIn') === 'true';
-      setIsSignIn(localStorageData && !!userInfo);
-    };
-
-    checkSignInStatus();
+    const localStorageData = localStorage.getItem('isSignIn') === 'true';
+    setIsSignIn(localStorageData);
   }, [userInfo]);
 
   const handleSignIn = () => {
     if (isSignIn) {
-      router.push(APP_ROUTES.mypage);
-    } else {
-      router.push(APP_ROUTES.signin);
+      openDropdown();
     }
   };
 
+  const handleSignOut = () => {
+    signout();
+    setIsSignIn(false);
+    closeDropdown();
+  };
   return (
-    <button onClick={handleSignIn} className="hidden md:flex">
-      {userInfo && isSignIn ? (
-        userInfo.image ? (
-          <Image
-            className="h-14 w-14 rounded-full border border-gray-200 object-cover"
-            src={userInfo.image}
-            alt="SIGN_IN_IMAGE"
-            width={40}
-            height={40}
-          />
+    <div className="relative hidden md:flex" ref={containerRef}>
+      <button onClick={handleSignIn}>
+        {userInfo && isSignIn ? (
+          userInfo.image ? (
+            <Image
+              className="h-14 w-14 rounded-full border border-gray-200 object-cover"
+              src={userInfo.image}
+              alt="SIGN_IN_IMAGE"
+              width={40}
+              height={40}
+            />
+          ) : (
+            <DefaultProfileImage width={40} height={40} />
+          )
         ) : (
-          <DefaultProfileImage width={40} height={40} />
-        )
-      ) : (
-        <span className="text-write-main text-base font-semibold">로그인</span>
+          <span className="text-write-main text-base font-semibold">로그인</span>
+        )}
+      </button>
+
+      {isDropdownOpen && (
+        <UserDropdown onSignOut={handleSignOut} onClose={closeDropdown} />
       )}
-    </button>
+    </div>
   );
 };
--- src/hooks/api/users/usePostSignout.ts
+++ src/hooks/api/users/usePostSignout.ts
@@ -1,13 +1,16 @@
 import { postSignOut } from '@/api/auth';
 import { deleteCookie } from '@/api/cookies';
 import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 
 export const usePostSignout = () => {
+  const router = useRouter();
   return useMutation({
     mutationFn: postSignOut,
     onSuccess: () => {
       deleteCookie('accessToken');
       localStorage.removeItem('isSignIn');
+      router.push('/auths/signin');
     },
     onError: (error) => {
       console.error(error);
