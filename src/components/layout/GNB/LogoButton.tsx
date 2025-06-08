be28f576cb6ff09be0f3486b79418a161bb18825
hoxey2Front <hoxey2react@gmail.com>
2025-06-08 22:57:00 +0900

    refactor: 로그인 버튼 onclick 속성 추가

--- src/components/layout/GNB/LogoButton.tsx
+++ src/components/layout/GNB/LogoButton.tsx
@@ -1,10 +1,13 @@
+import { LogoButtonProps } from '@/components/layout/SideDrawer/type';
 import { APP_ROUTES } from '@/constants/appRoutes';
 import Link from 'next/link';
 
-export const LogoButton = () => {
+export const LogoButton = ({ onClick }: LogoButtonProps) => {
   return (
     <h1 className="text-write-main font-hanuman pt-1.5 text-lg font-black lg:text-2xl lg:font-bold">
-      <Link href={APP_ROUTES.home}>WeWrite</Link>
+      <Link href={APP_ROUTES.home} onClick={onClick}>
+        WeWrite
+      </Link>
     </h1>
   );
 };
