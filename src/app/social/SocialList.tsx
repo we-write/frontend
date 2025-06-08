21ebca815e4b5230696ab168f310d7103a6fac3e
myeongheonhong <ruddyh7076@gmail.com>
2025-06-08 20:39:04 +0900

    style: 중복되는 스타일 제거

--- src/app/social/SocialList.tsx
+++ src/app/social/SocialList.tsx
@@ -16,7 +16,7 @@ const SocialListGrid = ({ socialList, isLoading }: SocialListGridProps) => {
   }
 
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
       {socialList.map((item) => (
         <GridCard
           key={item.id}
