37a52c910caac8832afc38719a7242199a2ac371
swimmingRiver <swimmingr@gmail.com>
2025-06-09 13:22:55 +0900

    fix: 로그인 모바일 사이즈 수치 조정

--- src/app/auths/signin/page.tsx
+++ src/app/auths/signin/page.tsx
@@ -46,7 +46,7 @@ const Page = () => {
 
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
-      <div className="flex max-h-[478px] w-[343px] flex-col gap-10 rounded-3xl bg-white px-4 py-6 sm:px-1 md:w-[608px] md:px-13.5 lg:max-h-[478px] lg:w-[508px]">
+      <div className="flex max-h-[478px] w-[343px] flex-col gap-10 rounded-3xl bg-white px-4 py-6 sm:px-4 md:w-[608px] md:px-13 lg:max-h-[478px] lg:w-[508px]">
         <h1 className="text-write-main text-center text-xl font-bold">
           로그인
         </h1>
