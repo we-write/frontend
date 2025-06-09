ad315de595da8e868c25db4947b8d5f487afcc06
hoxey2Front <159544052+hoxey2Front@users.noreply.github.com>
2025-06-09 13:41:28 +0900

    refactor: 필요 없는 코드제거, useClickOutside수정

--- src/components/layout/GNB/LoginSection.tsx
+++ src/components/layout/GNB/LoginSection.tsx
@@ -1,23 +1,16 @@
 'use client';
 
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 import { APP_ROUTES } from '../../../constants/appRoutes';
 import { DefaultProfileImage } from '@public/assets/icons';
 import useBoolean from '@/hooks/useBoolean';
-import { useClickOutside } from '@/hooks/useClickOutside';
+import useClickOutside from '@/hooks/useClickOutside';
 import { usePostSignout } from '@/hooks/api/users/usePostSignout';
 import UserDropdown from '@/components/layout/GNB/UserDropdown';
-import {
-  dehydrate,
-  HydrationBoundary,
-  useQueryClient,
-} from '@tanstack/react-query';
 import { LoginSectionProps } from '@/components/layout/GNB/type';
+import Link from 'next/link';
 
 const LoginSection = ({ isSignIn, userInfo }: LoginSectionProps) => {
-  const router = useRouter();
-  const queryClient = useQueryClient();
   const { mutate: signOut } = usePostSignout();
 
   const {
@@ -33,41 +26,39 @@ const LoginSection = ({ isSignIn, userInfo }: LoginSectionProps) => {
   };
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <div ref={ref} className="relative hidden md:flex">
-        {userInfo && isSignIn ? (
-          <button
-            onClick={openDropdown}
-            className="items-center justify-center"
-            aria-label="유저 메뉴 열기"
-          >
-            {userInfo.image ? (
-              <Image
-                className="h-14 w-14 rounded-full border border-gray-200 object-cover"
-                src={userInfo.image}
-                alt="프로필 이미지"
-                width={40}
-                height={40}
-              />
-            ) : (
-              <DefaultProfileImage width={40} height={40} />
-            )}
-          </button>
-        ) : (
-          <button
-            onClick={() => router.push(APP_ROUTES.signin)}
-            className="text-write-main text-base font-semibold"
-          >
-            로그인
-          </button>
-        )}
-        {isDropdownOpen && (
-          <div className="absolute top-full right-0 z-10">
-            <UserDropdown onSignOut={handleSignOut} onClose={closeDropdown} />
-          </div>
-        )}
-      </div>
-    </HydrationBoundary>
+    <div ref={ref} className="relative hidden md:flex">
+      {userInfo && isSignIn ? (
+        <button
+          onClick={openDropdown}
+          className="items-center justify-center"
+          aria-label="유저 메뉴 열기"
+        >
+          {userInfo.image ? (
+            <Image
+              className="h-14 w-14 rounded-full border border-gray-200 object-cover"
+              src={userInfo.image}
+              alt="프로필 이미지"
+              width={40}
+              height={40}
+            />
+          ) : (
+            <DefaultProfileImage width={40} height={40} />
+          )}
+        </button>
+      ) : (
+        <Link
+          href={APP_ROUTES.signin}
+          className="text-write-main text-base font-semibold"
+        >
+          로그인
+        </Link>
+      )}
+      {isDropdownOpen && (
+        <div className="absolute top-full right-0 z-10">
+          <UserDropdown onSignOut={handleSignOut} onClose={closeDropdown} />
+        </div>
+      )}
+    </div>
   );
 };
 
--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -1,19 +1,23 @@
 import { useEffect, useRef } from 'react';
 
-export const useClickOutside = (handler: () => void) => {
+const useClickOutside = (handler: () => void) => {
   const ref = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const listener = (event: MouseEvent) => {
+    const listener = (event: MouseEvent | TouchEvent) => {
       if (!ref.current || ref.current.contains(event.target as Node)) return;
       handler();
     };
 
     document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
     return () => {
       document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
     };
   }, [handler]);
 
   return ref;
 };
+export default useClickOutside;
