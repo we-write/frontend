f6d76e1a4d2b9060362709f4fd11e29c878dcef0
hoxey2Front <159544052+hoxey2Front@users.noreply.github.com>
2025-06-12 21:23:16 +0900

    feat: message 관련 const 제작

---
+++ src/constants/messages.ts
@@ -0,0 +1,10 @@
+export const SOCIAL_ACTION_MESSAGES = {
+  confirm: {
+    exit: '정말 모임을 나가시겠습니까?',
+    delete: '정말 모임을 삭제하시겠습니까?',
+  },
+  success: {
+    exit: '모임 나가기가 완료되었습니다.',
+    delete: '모임 삭제가 완료되었습니다.',
+  },
+};
