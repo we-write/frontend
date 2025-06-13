d23e7d645e1cbd4b20acd14dd0750ce56be0ecc9
swimmingRiver <swimmingr@gmail.com>
2025-06-13 17:07:32 +0900

    fix: 스토리 디테일 뒤로가기 경로 변경

--- src/app/library/detail/[id]/_components/SideButtonGroup.tsx
+++ src/app/library/detail/[id]/_components/SideButtonGroup.tsx
@@ -1,16 +1,16 @@
+import Button from '@/components/common/Button/Button';
 import { ChevronLeft } from 'lucide-react';
 // import { ChevronLeft, Heart } from 'lucide-react';
-import Link from 'next/link';
 
 const SideButtonGroup = () => {
   return (
     <div className="flex flex-row md:top-20 md:left-40 md:flex-col md:gap-4">
-      <Link
+      <Button
         className="flex-center md:border-write-main h-10 w-10 rounded-full border-0 bg-white md:h-12 md:w-12 md:border"
-        href="/library"
+        onClick={() => window.history.back()}
       >
         <ChevronLeft className="text-write-main h-6 w-6" />
-      </Link>
+      </Button>
       {/* <button
               className="flex-center md:border-write-main h-10 w-10 rounded-full border-0 bg-white md:h-12 md:w-12 md:border"
               onClick={() => router.push('/library')}
