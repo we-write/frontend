f1087be681e23cb2c603f9df2a10e447f1d4c67b
swimmingRiver <swimmingr@gmail.com>
2025-06-13 17:09:58 +0900

    fix: 버튼 디자인 수정

--- src/app/library/detail/[id]/_components/SideButtonGroup.tsx
+++ src/app/library/detail/[id]/_components/SideButtonGroup.tsx
@@ -1,4 +1,3 @@
-import Button from '@/components/common/Button/Button';
 import { ChevronLeft } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
@@ -6,12 +5,14 @@ const SideButtonGroup = () => {
   const router = useRouter();
   return (
     <div className="flex flex-row md:top-20 md:left-40 md:flex-col md:gap-4">
-      <Button
+      <button
+        type="button"
+        aria-label="뒤로가기"
         className="flex-center md:border-write-main h-10 w-10 rounded-full border-0 bg-white md:h-12 md:w-12 md:border"
         onClick={() => router.back()}
       >
         <ChevronLeft className="text-write-main h-6 w-6" />
-      </Button>
+      </button>
       {/* <button
               className="flex-center md:border-write-main h-10 w-10 rounded-full border-0 bg-white md:h-12 md:w-12 md:border"
               onClick={() => router.push('/library')}
