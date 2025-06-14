0b4ea5440ee4705161498bac37cc0235e846c609
juyesu <dong4757@naver.com>
2025-06-14 18:50:43 +0900

    refactor: 스토리 상세 페이지 연결 라우트 주소에 ?page=0 param을 추가

--- src/app/library/_components/LibraryListGrid.tsx
+++ src/app/library/_components/LibraryListGrid.tsx
@@ -66,7 +66,7 @@ const LibraryListGrid = ({
       <div className="gap-2: grid grid-cols-1 justify-items-center md:grid-cols-2 md:gap-4 lg:grid-cols-3">
         {flatStories?.map((story) => (
           <GridCard
-            href={`${APP_ROUTES.library}/detail/${story.story_id}`}
+            href={`${APP_ROUTES.library}/detail/${story.story_id}/?page=0`}
             key={story.story_id}
             image={{
               src:
