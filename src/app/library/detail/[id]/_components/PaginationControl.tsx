3f588c0b0a0362dd379f5a239685224412a9b8bf
juyesu <dong4757@naver.com>
2025-06-14 19:08:21 +0900

    refactor: 페이지 네이션을 상태 업데이트 방식에서 라우트 주소를 변경하는 방식으로 변경
    - 접근성 및 사용자 경험 개선을 위해 버튼에padding 적용 및 aria-label, aria-hidden 적용

--- src/app/library/detail/[id]/_components/PaginationControl.tsx
+++ src/app/library/detail/[id]/_components/PaginationControl.tsx
@@ -1,27 +1,56 @@
+'use client';
+
 import { ChevronLeft } from 'lucide-react';
 import { ChevronRight } from 'lucide-react';
 import { PaginationControlProps } from './type';
+import { useRouter } from 'next/navigation';
 
 export const PaginationControl = ({
+  storyId,
   title,
   page,
   totalPage,
-  setPage,
 }: PaginationControlProps) => {
+  const router = useRouter();
+  const changeNextPage = () => {
+    router.push(`/library/detail/${storyId}?page=${page + 1}`);
+  };
+
+  const changePrevPage = () => {
+    router.push(`/library/detail/${storyId}?page=${page - 1}`);
+  };
+
   return (
-    <div className="mt-4 mb-4 flex flex-col items-center justify-center gap-4">
-      <div>{title}</div>
-      <div className="flex items-center gap-4">
-        <button onClick={() => setPage(page - 1)} disabled={page < 1}>
-          <ChevronLeft className="h-4 w-4" />
+    <nav
+      className="fixed bottom-5 flex flex-col items-center justify-center gap-1.5 md:gap-4"
+      aria-label="페이지 이동 네비게이션"
+    >
+      <span
+        className={`font-semibold text-gray-600 ${title?.length > 30 && 'max-w-xs truncate'}`}
+      >
+        {title}
+      </span>
+      <div className="flex items-center gap-2">
+        <button
+          onClick={() => changePrevPage()}
+          disabled={page < 1}
+          className="p-3"
+          aria-label="이전 페이지로 이동"
+        >
+          <ChevronLeft className="h-4 w-4" aria-hidden="true" />
         </button>
         <span className="text-sm text-gray-400">
           {page} / {totalPage}
         </span>
-        <button onClick={() => setPage(page + 1)} disabled={page === totalPage}>
-          <ChevronRight className="h-4 w-4" />
+        <button
+          onClick={() => changeNextPage()}
+          disabled={page === totalPage}
+          className="p-3"
+          aria-label="다음 페이지로 이동"
+        >
+          <ChevronRight className="h-4 w-4" aria-hidden="true" />
         </button>
       </div>
-    </div>
+    </nav>
   );
 };
