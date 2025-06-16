6b5e134d9a0063732470a74e2dca5bc00da77283
swimmingRiver <swimmingr@gmail.com>
2025-06-16 11:34:46 +0900

    fix: userouter로 변경

--- src/app/library/detail/[id]/_components/SideButtonGroup.tsx
+++ src/app/library/detail/[id]/_components/SideButtonGroup.tsx
@@ -1,14 +1,17 @@
+'use client';
 import { ChevronLeft } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 // import { ChevronLeft, Heart } from 'lucide-react';
 
 const SideButtonGroup = () => {
+  const router = useRouter();
   return (
     <div className="flex flex-row md:top-20 md:left-40 md:flex-col md:gap-4">
       <button
         type="button"
         aria-label="뒤로가기"
         className="flex-center md:border-write-main h-10 w-10 rounded-full border-0 bg-white md:h-12 md:w-12 md:border"
-        onClick={() => window.history.back()}
+        onClick={() => router.back()}
       >
         <ChevronLeft className="text-write-main h-6 w-6" />
       </button>
