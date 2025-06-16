5f764c20c36547fda7bb8816b8321f4e7bbf8c35
juyesu <dong4757@naver.com>
2025-06-16 16:51:22 +0900

    refactor: contentCover 컴포넌트 내부에서 story인자에 사용된 옵셔널 체이닝 제거

--- src/app/library/detail/[id]/_components/ContentCover.tsx
+++ src/app/library/detail/[id]/_components/ContentCover.tsx
@@ -7,10 +7,10 @@ const ContentCover = ({ story }: { story: DBStoryResponse }) => {
   return (
     <div className="flex-center h-[80dvh] w-[95%] flex-col">
       <div className="relative aspect-square max-h-[50vh] min-h-[200px] w-[80%] max-w-[500px] min-w-[200px]">
-        {story?.cover_image_url && (
+        {story.cover_image_url && (
           <Image
-            src={story?.cover_image_url ?? ''}
-            alt={story?.title ?? '스토리 이미지'}
+            src={story.cover_image_url ?? ''}
+            alt={story.title ?? '스토리 이미지'}
             fill
             className="rounded-lg object-contain"
             priority
@@ -18,7 +18,7 @@ const ContentCover = ({ story }: { story: DBStoryResponse }) => {
         )}
       </div>
       <h1 className="mt-4 text-3xl font-semibold text-gray-800 md:text-[2rem]">
-        {story?.title}
+        {story.title}
       </h1>
     </div>
   );
