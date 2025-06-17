3a6e59d6d4376b130193a46df519ee63f6098fa7
swimmingRiver <swimmingr@gmail.com>
2025-06-17 15:39:08 +0900

    feat: 회원가입 토스트 추가

--- src/hooks/api/auth/useSignUpForm.ts
+++ src/hooks/api/auth/useSignUpForm.ts
@@ -4,6 +4,7 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { SignUpFormData } from '@/api/auth/type';
 import useCreateUser from '@/hooks/api/auth/useCreateUser';
 import { APP_ROUTES } from '@/constants/appRoutes';
+import toast from '@/utils/toast';
 
 const useSignUpForm = () => {
   const router = useRouter();
@@ -27,10 +28,16 @@ const useSignUpForm = () => {
 
     createUser(signUpData, {
       onSuccess: () => {
-        alert('회원가입이 완료되었습니다.');
+        toast.success('회원가입이 완료되었습니다.');
         router.push(APP_ROUTES.signin);
       },
       onError: (error: Error) => {
+        toast({
+          type: 'error',
+          title: '회원가입에 실패했습니다.',
+          message: error.message,
+          duration: 5,
+        });
         setError('email', {
           type: 'manual',
           message: error.message,
