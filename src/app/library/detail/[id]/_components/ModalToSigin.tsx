092aaf2c70ef4799b0cb7d3eb75cbc634ada6d13
swimmingRiver <swimmingr@gmail.com>
2025-06-18 14:04:03 +0900

    feat: 로그인 모달 추가

---
+++ src/app/library/detail/[id]/_components/ModalToSigin.tsx
@@ -0,0 +1,33 @@
+import Button from '@/components/common/Button/Button';
+import {
+  Modal,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from '@/components/common/Modal/Modal';
+import { APP_ROUTES } from '@/constants/appRoutes';
+import { useRouter } from 'next/navigation';
+
+const ModalToSigin = ({
+  isModalOpen,
+  setFalse,
+}: {
+  isModalOpen: boolean;
+  setFalse: () => void;
+}) => {
+  const router = useRouter();
+
+  return (
+    <Modal isOpen={isModalOpen} onClose={setFalse}>
+      <ModalHeader>스토리가 마음에 들었나요?</ModalHeader>
+      <ModalContent>로그인해서 좋아요를 눌러보세요</ModalContent>
+      <ModalFooter>
+        <Button onClick={() => router.push(APP_ROUTES.signin)}>
+          로그인하러 가기
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+export default ModalToSigin;
--- src/app/library/detail/[id]/_components/SideButtonGroup.tsx
+++ src/app/library/detail/[id]/_components/SideButtonGroup.tsx
@@ -5,17 +5,28 @@ import { useAuth } from '@/providers/auth-provider/AuthProvider.client';
 
 import useLikeStory from '@/hooks/api/library/useLikeStory';
 
+import useBoolean from '@/hooks/useBoolean';
+
+import ModalToSigin from './ModalToSigin';
 const SideButtonGroup = () => {
   const router = useRouter();
-  const { myInfo } = useAuth();
+  const { isSignIn, myInfo } = useAuth();
   const { id } = useParams();
+  const { value: isModalOpen, setTrue, setFalse } = useBoolean();
   const { handleLikeStory, isLiked, likeCount } = useLikeStory(
     id as string,
     myInfo?.id as number
   );
-
+  const handleClickLike = () => {
+    if (isSignIn) {
+      handleLikeStory();
+    } else {
+      setTrue();
+    }
+  };
   return (
     <div className="flex flex-row md:top-20 md:left-40 md:flex-col md:gap-4">
+      <ModalToSigin isModalOpen={isModalOpen} setFalse={setFalse} />
       <button
         aria-label="뒤로가기"
         className="flex-center md:border-write-main h-10 w-10 rounded-full border-0 bg-white md:h-12 md:w-12 md:border"
@@ -26,7 +37,7 @@ const SideButtonGroup = () => {
 
       <button
         className="flex-center md:border-write-main h-10 w-10 flex-col rounded-full border-0 bg-white md:h-12 md:w-12 md:border"
-        onClick={() => handleLikeStory()}
+        onClick={handleClickLike}
       >
         <Heart
           className="text-write-main h-6 w-6"
