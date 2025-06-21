24b66dc8f6339c0125bf34ce538440f8d68653ac
hoxey2Front <hoxey2react@gmail.com>
2025-06-21 17:08:48 +0900

    refactor: alert를 toast로 대체

--- src/hooks/api/auth/useUpdateMyInfo.ts
+++ src/hooks/api/auth/useUpdateMyInfo.ts
@@ -5,6 +5,7 @@ import { updateMyInfo } from '@/api/auth/api';
 import { MyInfoRequest } from '@/api/auth/type';
 import { QUERY_KEY } from '@/constants/queryKey';
 import { useRouter } from 'next/navigation';
+import toast from '@/utils/toast';
 
 export const useUpdateMyInfo = () => {
   // const queryClient = useQueryClient();
@@ -14,8 +15,16 @@ export const useUpdateMyInfo = () => {
     mutationKey: [QUERY_KEY.MY_INFO],
     mutationFn: (data: MyInfoRequest) => updateMyInfo(data),
     onSuccess: () => {
+      toast.success('프로필이 수정되었습니다.');
       router.refresh();
       // queryClient.invalidateQueries({ queryKey: [QUERY_KEY.MY_INFO] });
+    },
+    onError: () => {
+      toast({
+        type: 'error',
+        message: '오류가 발생하여 프로필 수정에 실패하였습니다.',
+        duration: 5,
+      });
     },
   });
 };
