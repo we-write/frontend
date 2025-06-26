4907eca7dede0c64ef1f0442e387319ff729bd47
juyesu <163122800+juyesu@users.noreply.github.com>
2025-06-26 23:37:39 +0900

    chore: main에 병합된 Hotfix/#285 변경사항을 develop 브랜치에 반영

--- src/app/library/detail/[id]/_components/StoryModalTriggerButton.tsx
+++ src/app/library/detail/[id]/_components/StoryModalTriggerButton.tsx
@@ -1,11 +1,22 @@
+import { QUERY_KEY } from '@/constants/queryKey';
+import { getQueryClient } from '@/lib/queryClinet';
 import { useStoryModal } from '@/providers/StoryWriteOrApproveModalProviders';
 import { ContentWriteIcon } from '@public/assets/icons';
 
-const StoryModalTriggerButton = () => {
+const StoryModalTriggerButton = ({ storyId }: { storyId: string }) => {
   const { openModal } = useStoryModal();
+  const queryClient = getQueryClient();
+
+  const handleOpenModal = () => {
+    queryClient.invalidateQueries({
+      queryKey: [QUERY_KEY.GET_LAST_CONTENT, storyId],
+    });
+    openModal();
+  };
+
   return (
     <button
-      onClick={openModal}
+      onClick={handleOpenModal}
       className="fixed right-6 bottom-10 h-15 w-15 md:right-22 md:bottom-14"
     >
       <ContentWriteIcon aria-hidden="true" />
