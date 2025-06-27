63bffbcdd38b4efc0d0e7133991257382fe262a6
swimmingRiver <swimmingr@gmail.com>
2025-06-27 14:32:42 +0900

    fix: 충돌 해결 packagejson jestconfig

--- jest.config.ts
+++ jest.config.ts
@@ -5,12 +5,10 @@ const createJestConfig = nextJest({
   dir: './',
 });
 
-
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
-
 
 export default createJestConfig(config);
