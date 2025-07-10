446ab41b81eac356d2eceafc52ac21b9fd056d86
hoxey2react <hoxey2react@gmail.com>
2025-07-10 23:18:34 +0900

    feat: favicon 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ const hanuman = Hanuman({
 export const metadata: Metadata = {
   title: 'We Write',
   description: 'We Write',
+  icons: {
+    icon: '/assets/icons/Favicon.svg',
+  },
 };
 
 export const viewport: Viewport = {
