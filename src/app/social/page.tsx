e8bf6365db0402439479ee6ce34f0600d8580c99
juyesu <dong4757@naver.com>
2025-07-17 18:00:56 +0900

    refactor: 미들웨어 적용에 따라 각 컴포넌트에서 리다이렉트하는 로직을 삭제, 리다이렉트 시 토스트를 출력하는 ClientSideHandler 컴포넌트 호출 추가

--- src/app/social/page.tsx
+++ src/app/social/page.tsx
@@ -5,6 +5,7 @@ import { getQueryClient } from '@/lib/queryClinet';
 import SocialListContainer from './_components/SocialListContainer';
 import { BookOpen } from 'lucide-react';
 import type { Metadata } from 'next';
+import ClientSideHandler from '@/components/logic/ClientSideHandler';
 
 export const metadata: Metadata = {
   title: '스토리그룹 - WeWrite',
@@ -35,23 +36,26 @@ const Social = async () => {
   });
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <div className="flex flex-col justify-center gap-5 px-3 py-8 md:px-7">
-        <h1 className="text-xl leading-tight font-medium text-gray-900 md:text-[1.5rem]">
-          모두와 만들어 나가는 상상의 여정
-          <span className="text-write-sub-title mt-1 block text-[2.4rem] font-extrabold">
-            함께 쓰고, 함께 완성하는 이야기
-            <span className="text-write-sub-title ml-2 inline-block pt-1.5 align-top">
-              <BookOpen className="h-6.5 w-6.5" aria-hidden="true" />
+    <>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <div className="flex flex-col justify-center gap-5 px-3 py-8 md:px-7">
+          <h1 className="text-xl leading-tight font-medium text-gray-900 md:text-[1.5rem]">
+            모두와 만들어 나가는 상상의 여정
+            <span className="text-write-sub-title mt-1 block text-[2.4rem] font-extrabold">
+              함께 쓰고, 함께 완성하는 이야기
+              <span className="text-write-sub-title ml-2 inline-block pt-1.5 align-top">
+                <BookOpen className="h-6.5 w-6.5" aria-hidden="true" />
+              </span>
             </span>
-          </span>
-        </h1>
-        <p className="text-lg font-medium text-gray-400 md:text-lg">
-          지금, 당신의 상상을 더해보세요
-        </p>
-      </div>
-      <SocialListContainer />
-    </HydrationBoundary>
+          </h1>
+          <p className="text-lg font-medium text-gray-400 md:text-lg">
+            지금, 당신의 상상을 더해보세요
+          </p>
+        </div>
+        <SocialListContainer />
+      </HydrationBoundary>
+      <ClientSideHandler />
+    </>
   );
 };
 
